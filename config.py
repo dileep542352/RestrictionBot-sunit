59e4fc12e39d15368dcf15306edebe7e25148c4d
songsunmc <118031927+dileep542352@users.noreply.github.com>
2025-01-31 16:45:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "27256080"))
-API_HASH = getenv("API_HASH", "0603cf221dfc7bea62656d11a57f9e41")
+API_ID = int(getenv("API_ID", "23694600"))
+API_HASH = getenv("API_HASH", "7bf5cc011eeab9270463dbb194689b51")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5565741405 6107581019 8001147497").split()))
-MONGO_DB = getenv("MONGO_DB", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6879123652").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://sonudohare:chodubaksa@cluster0.bhh3z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
 PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", ""))
