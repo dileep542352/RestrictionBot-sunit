46165d2fa6dd3240a4804e3ebed60aa93ee43b4b
songsunmc <118031927+dileep542352@users.noreply.github.com>
2025-01-31 18:30:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "6879123652").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://sonudohare:chodubaksa@cluster0.bhh3z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "1002290479633"))
 PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", ""))
 
 
