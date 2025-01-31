7e14506e2c5c7b3ac021b57182c877f34f413ecd
songsunmc <118031927+dileep542352@users.noreply.github.com>
2025-01-31 21:17:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,7 @@ from os import getenv
 API_ID = int(getenv("API_ID", "23694600"))
 API_HASH = getenv("API_HASH", "7bf5cc011eeab9270463dbb194689b51")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "6879123652").split()))
-MONGO_DB = getenv("MONGO_DB", "mongodb+srv://sonudohare:chodubaksa@cluster0.bhh3z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+AUTH = list(map(int, getenv("AUTH", "6879123652").split()))
+SESSION = getenv("SESSION", "")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", ""))
-
-
+FORCESUB = int(getenv("FORCESUB", ""))
